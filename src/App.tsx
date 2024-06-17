01683ca758e24acf6c40d24c8eed5270013f2cef
VyeoooT <vlinh52000@gmail.com>
2024-06-17 20:35:10 +0700

    - Inittial src

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-gray-800 font-bold">inittial profile</div>
+    </>
+  )
+}
+
+export default App
